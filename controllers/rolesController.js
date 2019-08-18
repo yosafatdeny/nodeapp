005bcec4098db3de27e5b9bb2444739bde8cfe07
hisbu <lab.hisbu@gmail.com>
2019-08-18 23:08:10 +0700

    add roles

--- controllers/rolesController.js
+++ controllers/rolesController.js
@@ -9,10 +9,25 @@ module.exports = {
             console.log(result)
             return res.status(200).send(result)
         })
-    }
-    // addRole: (req, res)=>{
-    //     try{
+    },
+    addRole: (req, res)=>{
+        try{
+            console.log(req.body)
+            var sql = `INSERT INTO roles SET ?`
+            conn.query(sql, data, (err, results)=>{
+                if(err) return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message });
 
-    //     }
-    // }
+                console.log(results)
+                sql = `SELECT * FROM roles;`
+                conn.query(sql, (err, result2)=>{
+                    if(err) return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
+
+                    console.log(result2)
+                    return res.status(200).send(result2)
+                })
+            })
+        }catch(err){
+            return res.status(500).json({ message: "There's an error on the server. Please contact the administrator.", error: err.message })
+        }
+    }
 }
--- routers/rolesRouter.js
+++ routers/rolesRouter.js
@@ -4,5 +4,6 @@ const { rolesController } = require('../controllers')
 const router = express.Router()
 
 router.get('/getRoles', rolesController.getRoles)
+router.post('/addRole', rolesController.addRole)
 
 module.exports = router
