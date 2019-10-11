521867e610f10a02df8276e710608d60dd748551
hisbu <lab.hisbu@gmail.com>
2019-10-11 17:13:35 +0700

    m

--- database/index.js
+++ database/index.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 
 const conn = mysql.createConnection({
     host    : 'localhost',
-    user    : 'hisbu',
-    password: 'P@ssw0rd',
-    database: 'dbqelas',
+    user    : 'saitama',
+    password: 'Pl163178149;',
+    database: 'hisbu_dbqelas',
     port    : 3306
 
     
