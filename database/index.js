3383ecafae4100392e1294e5b5bc474bdf9e4f1c
hisbu <lab.hisbu@gmail.com>
2019-10-11 15:40:30 +0700

    m

--- database/index.js
+++ database/index.js
@@ -1,21 +1,21 @@
 const mysql = require('mysql')
 
-// const conn = mysql.createConnection({
-//     host    : 'localhost',
-//     user    : 'hisbu',
-//     password: 'P@ssw0rd',
-//     database: 'dbqelas',
-//     port    : 3306
+const conn = mysql.createConnection({
+    host    : 'localhost',
+    user    : 'saitama',
+    password: 'Pl163178149;',
+    database: 'hisbu_dbqelas',
+    port    : 3306
 
     
-// })
-
-const conn = mysql.createConnection({
-    host    : 'db4free.net',
-    user    : 'hisbu44',
-    password: 'P@ssw0rd.o1',
-    database: 'devapi'
 })
+
+// const conn = mysql.createConnection({
+//     host    : 'db4free.net',
+//     user    : 'hisbu44',
+//     password: 'P@ssw0rd.o1',
+//     database: 'devapi'
+// })
 
 
 
